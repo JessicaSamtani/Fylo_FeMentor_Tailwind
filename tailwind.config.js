c1671de6183f18513fc6a42142bfcf05e2cf9845
Jessica Samtani <jessicasamtani@gmail.com>
2023-10-12 17:01:50 +0700

    feat: submitted changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        paleBlue: "hsl(243, 100%, 93%)",
+        grayishBlue: "hsl(229, 7%, 55%)",
+        darkBlue: "hsl(228, 56%, 26%)",
+        veryDarkBlue: "hsl(229, 57%, 11%)",
+      },
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+      },
+
+      backgroundImage: {
+        desktopImage: "url('./images/bg-desktop.png')",
+        mobileImage: "url('./images/bg-mobile.png')",
+      },
+    },
   },
   plugins: [],
-}
+};
